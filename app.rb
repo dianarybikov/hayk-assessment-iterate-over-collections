79cb4d32adda70f748023dbe0fe3c5352bd4c898
Diana Rybikov <diana.ryb214@gmail.com>
2020-12-15 11:09:41 -0700

    commit tree 4135fc0c91bc962d7a89f1403c90bb7ac3e312f3

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Company
     attr_accessor :name, :size
 
@@ -14,3 +15,37 @@ companies = [
 ]
 
 ### Your code below this
+
+# Generate a list of strings with the name and size (eg. "Alpha - 30")
+# * Combine all the sizes with reduce
+# * Filter the list to show only companies over 100
+# * Find the company named "Beta"
+# * Find the largest company
+# * Sort the companies from largest to smallest
+def combine_all_sizes(company)
+    company.reduce(0) do |companies|
+        companies.size
+        binding.pry
+    end
+    end
+
+    def companies_over_one_hundred(company)
+        company.filter do |size|
+            size > 100
+        end
+        end
+        
+    def find_company_name(company)
+        company.find do |companies|
+            companies.include?(Beta)
+        end
+    end
+    def find_largest_company(company)
+        company.find do |companies|
+            companies[@size].max
+        end
+    end
+    
+    def largest_to_smallest(company)
+        company.sort
+    end
